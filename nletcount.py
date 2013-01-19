c1727cc38db6966b1b7dd6d07dbb3a365f8c12ab
Phillip Nordwall <Phillip.Nordwall+github@gmail.com>
2013-01-18 22:15:16 -0800

    Added arg parsing. Got rid of filter pos counts.

--- nletcount.py
+++ nletcount.py
@@ -6,11 +6,13 @@ characters in a given file, as well as frequency accounts for singlets
 from collections import defaultdict
 from itertools import islice
 from sys import exit
+from optparse import OptionParser
 
 def window(seq, window_width=1):
   "Returns a sliding window (of width window_width) over data from the iterable"
   "   s -> (s0,s1,...s[window_width-1]), (s1,s2,...,swindow_width), ...        "
-  result = tuple(islice(iter(seq), window_width))
+  it = iter(seq)
+  result = tuple(islice(it, window_width))
   if len(result) == window_width:
     yield result    
   for elem in it:
@@ -19,25 +21,37 @@ def window(seq, window_width=1):
 
 def nletcount(filename, window_width = 2):
   counts = defaultdict(int)
-  with open("enwords") as f:
+  with open("enwords", "rb") as f:
     for pair in window(iter(lambda:f.read(1), ""), window_width):
       counts[("".join(pair)).lower()]+=1
   return sorted([(j,i) for (i,j) in counts.items()])
 
 def main():
-  scounts = nletcount("enwords", 1)
-  pcounts = nletcount("enwords", 2)
-  d_counts = filter(lambda x: 'd' in x[1], pcounts)
-  f_dcounts = filter(lambda x: 'd' == x[1][1], pcounts)
-  s_dcounts = filter(lambda x: 'd' == x[1][0], pcounts)
+  use = """Usage: %prog [options] filename"""
+  ver = "%prog 0.1"
+  parser = OptionParser(usage=use, version=ver)
+  parser.add_option("-w", "--window-width",
+                    default = 2,
+                    type = "int",
+                    dest = "window_width",
+                    help = "Tally counts of window_width lengthed substrings")
+  parser.add_option("-f", "--filter",
+                    dest = "filt",
+                    help = "show only counts that contain filt")
+  (options, args) = parser.parse_args()
+
+  if len(args) != 1:
+    parser.error("incorrect number of arguments")
+  filename = args[0]
+
+  scounts = nletcount(filename, 1)
+  pcounts = nletcount(filename, options.window_width)
   print "letter counts"
   print "\n".join(map(repr, scounts))
-  print "pairs that contain d"
-  print "\n".join(map(repr, d_counts))
-  print "\npairs start with d"
-  print "\n".join(map(repr, f_dcounts))
-  print "\npairs end with d"
-  print "\n".join(map(repr, s_dcounts))
+  if options.filt:
+    f_counts = filter(lambda x: options.filt.lower() in x[1], pcounts)
+    print "pairs that contain %s" % options.filt
+    print "\n".join(map(repr, f_counts))
 
 if __name__ == "__main__":
   exit(main())
